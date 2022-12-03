a743de4da6d28ec572cbdbe90ea18dbb1d24596b
Cocoa <r.xu.1@research.gla.ac.uk>
2022-12-03 13:59:05 +0000

    updated day 3 code

--- day3/day3.py
+++ day3/day3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import functools
+from functools import reduce
 
 
 if __name__ == '__main__':
@@ -11,21 +11,18 @@ if __name__ == '__main__':
         index = 0
         group_items = []
         get_priority = lambda c: ord(c) - ord('A') + 27 if ord(c) <= ord('Z') else ord(c) - ord('a') + 1
-        foldl = lambda func, acc, xs: functools.reduce(func, xs, acc)
         for line in f:
             line = line.strip()
             num_items = len(line) // 2
             first_half = set(line[:num_items])
             second_half = set(line[num_items:])
+            score_part1.append(get_priority(list(first_half.intersection(second_half))[0]))
+            
             index += 1
             group_items.append(set(line))
             if index % 3 == 0:
-                item = list(foldl(lambda a, b: a.intersection(b), group_items[0], group_items))[0]
+                score_part2.append(get_priority(list(reduce(set.intersection, group_items, group_items[0]))[0]))
                 group_items = []
-                score_part2.append(get_priority(item))
-
-            common = list(first_half.intersection(second_half))[0]
-            score_part1.append(get_priority(common))
             
         print(sum(score_part1))
         print(sum(score_part2))
